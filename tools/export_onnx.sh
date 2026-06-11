b61f5de67ed370bce14bdb3e34d06bd9a2f33f5c
Nongshim USA <deokpark@nongshimusa.com>
2026-06-11 11:17:44 +0900

    fix: disable PaddleX model source check to prevent paddleocr import failure

--- tools/export_onnx.sh
+++ tools/export_onnx.sh
@@ -12,6 +12,8 @@
 RED='\033[0;31m'; GREEN='\033[0;32m'; YELLOW='\033[1;33m'
 CYAN='\033[0;36m'; BOLD='\033[1m'; RESET='\033[0m'
 
+export PADDLE_PDX_DISABLE_MODEL_SOURCE_CHECK=True
+
 REPO="GHengoy/NS_runtime"
 RELEASE_TAG="onnx-models-v1"
 OUTPUT_BASE="./onnx_export"
